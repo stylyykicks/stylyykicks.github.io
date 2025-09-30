5f55981027fb97510956cdbd08860231e024dbbd
Muhammad Nafil E <nafilnafilnafil231@gmail.com>
2025-09-30 20:26:25 +0530

    Bugg Fixes- Responsive

--- js/script.js
+++ js/script.js
@@ -90,7 +90,7 @@ document.addEventListener("DOMContentLoaded", () => {
             <h3 class="font-bold text-lg">Selection Incomplete</h3>
             <p class="mt-2 text-slate-600">${message}</p>
             <div class="mt-6 flex justify-center">
-                <button id="go-to-offer" class="bg-blue-600 text-white px-4 py-2 rounded-md">Go to Offer Page</button>
+                <button id="go-to-offer" class="bg-blue-600 text-white px-4 py-2 rounded-md">Offer Page</button>
             </div>
         </div>
     `;
